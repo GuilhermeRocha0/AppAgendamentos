c03ca08639a89aca6855b088479a368e0baf8803
Sekioll <you@example.com>
2021-10-28 20:32:14 -0300

    init

---
+++ www/js/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyDsCrw_SiAlhcZ3o-UcOehQe0Igy0waVec",
+    authDomain: "app-agendamentos-15fa9.firebaseapp.com",
+    projectId: "app-agendamentos-15fa9",
+    storageBucket: "app-agendamentos-15fa9.appspot.com",
+    messagingSenderId: "595214280693",
+    appId: "1:595214280693:web:67e14a0bf9c999d4b762bc",
+    measurementId: "G-935EXJ3Y3E"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
